391c15d3721a488c6bcd046c65c1e84d3c8f3ea1
unknown <igormcoimbra@gmail.com>
2023-11-30 21:27:34 +0000

    api call for 10 results

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,32 +1,40 @@
+var apiKey = prompt('Enter your API key:');
+var queryURL = "https://api.spoonacular.com/recipes/complexSearch";
+var apiUrl = "https://api.spoonacular.com/recipes/complexSearch?query=pasta"+"&includeNutrition=true"+"&apiKey="+apiKey;
+
+function fetchRecipe() {
 
 
+  console.log(apiUrl);
 
-function fetchRecipe(url) {
-    var apiKey = prompt('Enter your API key:');
-  
-    if (!apiKey) {
-      console.error('Please enter a valid API key');
-      return;
-    }
+  if (!apiKey) {
+    console.error('Please enter a valid API key');
+    return;
+  }
 
-    var headers = {
-        'Authorization': `Bearer ${apiKey}`
-      };
-    
-      var request = new XMLHttpRequest();
-      request.open('GET', url, true);
-      request.setRequestHeader('Authorization', `Bearer ${apiKey}`);
-      request.onload = function() {
-        if (request.status === 200) {
-            var data = JSON.parse(request.responseText);
-            console.log(data);
-          } else {
-            throw new Error(`Error fetching data: ${request.status}`);
-          }
-        };
-        request.send();
-      }
+  const headers = {
+    'Authorization': `Bearer ${apiKey}`
+  };
 
-      var apiUrl = "https://api.spoonacular.com/recipes/716429/information?apiKey="+apiKey +"&includeNutrition=true";
+  fetch(apiUrl, { headers })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    // .catch(error => console.error('Error fetching recipe:', error));
+}
+      // var request = new XMLHttpRequest();
+      // request.open('GET', url, true);
+      // request.setRequestHeader('Authorization', `Bearer ${apiKey}`);
+      // request.onload = function() {
+      //   if (request.status === 200) {
+      //       var data = JSON.parse(request.responseText);
+      //       console.log(data);
+      //     } else {
+      //       throw new Error(`Error fetching data: ${request.status}`);
+      //     }
+      //   };
+      //   request.send();
+      // }
 
-      fetchRecipe();
+      
+
+       fetchRecipe(apiUrl);
