939566dceacb79ab25e78dc2c6450c7933038f51
pineapple laptop <martinkosel@yahoo.de>
2023-12-01 18:20:20 +0000

    Adds basic styling

---
+++ assets/js/logic.js
@@ -0,0 +1,11 @@
+// Heart button function
+
+jQuery(function($) {
+    $('#swapHeart').on('click', function() {
+      var $el = $(this),
+        textNode = this.lastChild;
+      $el.find('span').toggleClass('glyphicon-heart glyphicon-heart-empty');
+      $el.toggleClass('swap');
+    });
+  });
+
